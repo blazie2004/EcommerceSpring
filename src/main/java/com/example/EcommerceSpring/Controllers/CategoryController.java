78653a0312092c8e36986b6e212ed6d8bb36963c
jay <jayvc1703@gmail.com>
2026-01-15 16:43:47 +0530

    added controller and service layer basics

---
+++ src/main/java/com/example/EcommerceSpring/Controllers/CategoryController.java
@@ -0,0 +1,31 @@
+package com.example.EcommerceSpring.Controllers;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+@RestController
+
+@RequestMapping("/api/categories")
+public class CategoryController {
+
+//    @GetMapping
+//    public String getCategory(){
+//        return "electronics";
+//    }
+//    @GetMapping("/count")
+//    public int getCount(){
+//        return 5;
+//    }
+
+
+
+
+
+}
---
+++ src/main/java/com/example/EcommerceSpring/Services/CategoryService.java
@@ -0,0 +1,5 @@
+package com.example.EcommerceSpring.Services;
+
+public class CategoryService {
+
+}
